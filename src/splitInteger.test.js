5783e8a9aa3de537bf69733ab63968ba011a9814
Anastasia <anvo@cloudprinter.com>
2023-08-03 13:47:51 +0200

    done

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -2,20 +2,26 @@
 
 const splitInteger = require('./splitInteger');
 
-test(`should split a number into equal parts
-  if a value is divisible by a numberOfParts`, () => {
+test(`should split a number into equal parts if a value is divisible by a numberOfParts`, () => {
+  const result = splitInteger(8, 4);
 
+  expect(result).toEqual([ 2, 2, 2, 2 ]);
 });
 
-test(`should return a part equals to a value
-  when splitting into 1 part`, () => {
+test(`should return a part equals to a value when splitting into 1 part`, () => {
+  const result = splitInteger(333, 1);
 
+  expect(result).toEqual([333]);
 });
 
 test('should sort parts ascending if they are not equal', () => {
+  const result = splitInteger(66, 5);
 
+  expect(result).toEqual([ 13, 13, 13, 13, 14 ]);
 });
 
 test('should add zeros if value < numberOfParts', () => {
+  const result = splitInteger(2, 3);
 
+  expect(result).toEqual([0, 1, 1]);
 });
